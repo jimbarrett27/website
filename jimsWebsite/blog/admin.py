d4722bcd3c7f6c19baba627e6c312b10c33f9891
Jim Barrett <jim.barrett@klarna.com>
2018-07-20 16:51:03 +0100

    finished following the Mozilla tutorial

--- jimsWebsite/blog/admin.py
+++ jimsWebsite/blog/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Blog, BlogPost, Author, Topic
 
 # Register your models here.
+admin.site.register(Blog)
+admin.site.register(BlogPost)
+admin.site.register(Author)
+admin.site.register(Topic)
