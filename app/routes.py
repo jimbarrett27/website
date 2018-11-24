d098e94dbf1b05eb4ceb6ced93160eed8697fb28
Jim Barrett <jimbarrett27@gmail.com>
2018-11-24 15:40:33 +0100

    successfully copied over and linked up the content

--- app/routes.py
+++ app/routes.py
@@ -4,15 +4,4 @@ from flask import render_template
 @app.route('/')
 @app.route('/index')
 def index():
-    user = {'username': 'Jim'}
-    posts = [
-        {
-            'author': {'username': 'Jim1'},
-            'body': 'Oh fuck!!'
-        },
-        {
-            'author': {'username': 'Jim2'},
-            'body': 'It works!!'
-        }
-    ]
-    return render_template('index.html', title='Home', user=user, posts=posts)
+    return render_template('frontPage.html')
