7270822cad64aaf3252498002725efdf154d2e09
Jim Barrett <jimbarrett27@gmail.com>
2018-12-01 15:25:25 +0100

    adding new page to site

--- app/routes.py
+++ app/routes.py
@@ -9,3 +9,7 @@ def frontPage():
 @app.route('/activity')
 def activity():
 	return render_template('activity.html')
+
+@app.route('/projectEuler')
+def projectEuler():
+	return render_template('projectEuler.html')
