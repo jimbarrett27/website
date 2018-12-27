8f5f28dedc2543a05eb993e0e334c65b63a71c2e
Jim Barrett <jimbarrett27@gmail.com>
2018-12-27 23:08:54 +0000

    solution to exercise 11

---
+++ app/static/js/exercise11.js
@@ -0,0 +1,75 @@
+import {loadFile} from "./fileLoading.js"
+
+function exercise11()
+{
+	const inputData = loadFile('/static/js/data.txt').split("\n");
+	
+	var grid = []
+	for (let row of inputData)
+	{
+		grid.push(row.split(" "))
+	}
+
+	let biggest = -1;
+
+	// rows
+	for (let i=0; i<grid.length; i++)
+	{
+		for (let j=0; j<grid[0].length - 4; j++)
+		{
+			let product = 1;
+			for (let k=0; k<4; k++)
+			{
+				product *= grid[i][j+k]
+			}
+			if (product > biggest) biggest = product;
+		}
+	}
+
+	// columns
+	for (let i=0; i<grid.length; i++)
+	{
+		for (let j=0; j<grid[0].length - 4; j++)
+		{
+			let product = 1;
+			for (let k=0; k<4; k++)
+			{
+				product *= grid[j+k][i]
+			}
+			if (product > biggest) biggest = product;
+		}
+	}
+
+	// UL-DR diagonals
+	for (let i=0; i<grid.length - 4; i++)
+	{
+		for (let j=0; j<grid[0].length - 4; j++)
+		{
+			let product = 1;
+			for (let k=0; k<4; k++)
+			{
+				product *= grid[i+k][j+k]
+			}
+			if (product > biggest) biggest = product;
+		}
+	}
+
+	// UR-DL diagonals
+	for (let i=4; i<grid.length; i++)
+	{
+		for (let j=0; j<grid[0].length - 4; j++)
+		{
+			let product = 1;
+			for (let k=0; k<4; k++)
+			{
+				product *= grid[i-k][j+k]
+			}
+			if (product > biggest) biggest = product;
+		}
+	}
+
+	return biggest;
+
+}
+
+export { exercise11 as default }
