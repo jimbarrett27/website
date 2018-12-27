d058fff5a539d87b39c8a6cddf785876446ff4e0
Jim Barrett <jimbarrett27@gmail.com>
2018-12-27 22:36:23 +0000

    module for loading in static text files

---
+++ website/app/static/js/fileLoading.js
@@ -0,0 +1,18 @@
+function loadFile(path)
+{
+	var fileContents;
+
+	function listener()
+	{
+		fileContents = this.responseText;
+	}
+	
+	var req = new XMLHttpRequest();
+	req.addEventListener("load", listener);
+	req.open('GET', path, false);
+	req.send();
+
+	return fileContents;
+}
+
+export { loadFile }
