ff81306004de34ad9628558dca238ae290a26002
Jim Barrett <jim.barrett@klarna.com>
2019-11-16 19:09:10 +0100

    slightly more interesting bot

--- website/twitter/dataguybot.py
+++ website/twitter/dataguybot.py
@@ -1,12 +1,17 @@
 import tweepy
 import os
 from dataclasses import make_dataclass
-import datetime
+from pathlib import Path
+import numpy
 
 TwitterCredentials = make_dataclass('TwitterCredentials', ['consumer', 'consumer_secret', 'access', 'access_secret'])
 
 twitter_credentials = TwitterCredentials(os.environ.get('TWITTER_CONSUMER'), os.environ.get('TWITTER_CONSUMER_SECRET'), os.environ.get('TWITTER_ACCESS'), os.environ.get('TWITTER_ACCESS_SECRET'))
 
+current_dir = Path().resolve()
+
+with open(current_dir / "positive_adjectives.txt", 'r') as f:
+    adjectives = [l for l in f]
 
 auth = tweepy.OAuthHandler(twitter_credentials.consumer, twitter_credentials.consumer_secret)
 auth.set_access_token(twitter_credentials.access, twitter_credentials.access_secret)
@@ -15,6 +20,6 @@ api = tweepy.API(auth)
 
 try:
     api.verify_credentials()
-    api.update_status(f"Bleep Bloop! The time is {datetime.datetime.now()}")
+    api.update_status(f"My favourite data is {np.random.choice(adjectives)} data")
 except:
     print("Something went wrong with authentication!")
