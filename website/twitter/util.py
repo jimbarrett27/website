1ab4b6900fee274ef68de7158283c5e1cbbf37a2
Jim Barrett <jimbarrett27@gmail.com>
2019-11-30 15:11:57 +0100

    refactoring out some of the boilerplate

---
+++ website/twitter/util.py
@@ -0,0 +1,21 @@
+"""
+Some handy utility function for working with twitter
+"""
+import tweepy
+from twitter.credentials import CREDENTIALS
+
+def get_tweepy_api():
+    """
+    Authorises and constructs the tweepy api object
+    """
+
+    auth = tweepy.OAuthHandler(
+        CREDENTIALS.consumer, CREDENTIALS.consumer_secret
+    )
+    auth.set_access_token(CREDENTIALS.access, CREDENTIALS.access_secret)
+
+    api = tweepy.API(auth)
+
+    api.verify_credentials()
+
+    return api
