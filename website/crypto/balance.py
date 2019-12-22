3263c07451dccf7295172ae0f9d137223085dfa7
Jim Barrett <jim.barrett@klarna.com>
2019-12-22 18:07:04 +0000

    linting

--- website/crypto/balance.py
+++ website/crypto/balance.py
@@ -1,20 +1,28 @@
-from crypto.client import get_authenticated_client
-from constants import TRADED_COINS
+"""
+Functions for expressing the current balances
+"""
 
-def get_current_balance(against='USDT'):
+from constants import TRADED_COINS
+from crypto.client import get_authenticated_client
+
+
+def get_current_balance(against="USDT"):
+    """
+    Returns my current total value in the "against" market
+    """
 
     client = get_authenticated_client()
     prices = client.get_all_tickers()
 
-    relevant_prices = {price['symbol'][:-4]:float(price['price']) for price in prices if price['symbol'].endswith('USDT') and price['symbol'][:-4] in TRADED_COINS}
+    relevant_prices = {
+        price["symbol"][:-4]: float(price["price"])
+        for price in prices
+        if price["symbol"].endswith(against) and price["symbol"][:-4] in TRADED_COINS
+    }
 
     total = 0
     for coin in TRADED_COINS:
-        balance = float(client.get_asset_balance(coin)['free'])
+        balance = float(client.get_asset_balance(coin)["free"])
         total += relevant_prices[coin] * balance
 
     return total
-
-
-
-
