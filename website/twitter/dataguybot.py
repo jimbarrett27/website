8b1a9d794b24387679d144b03edac96df2757feb
Jim Barrett <jim.barrett@klarna.com>
2019-12-23 20:44:43 +0000

    current balance tweetin

--- website/twitter/dataguybot.py
+++ website/twitter/dataguybot.py
@@ -2,10 +2,13 @@
 Entry point to the twitter bot
 """
 
+from datetime import datetime
 from pathlib import Path
+from tempfile import NamedTemporaryFile
 
 import numpy as np
 
+from crypto.balance import get_current_balance
 from twitter.util import get_tweepy_api
 
 
@@ -19,7 +22,18 @@ def dataguybot_tweet():
 
     api = get_tweepy_api()
 
-    api.update_status(f"My favourite data is {np.random.choice(adjectives)} data")
+    current_hour = datetime.now().hour + 1
+    if current_hour % 24 == 0:
+        api.update_status(f"My favourite data is {np.random.choice(adjectives)} data")
+    if current_hour % 4 == 0:
+        with NamedTemporaryFile() as f:
+            plot_filepath = f"{f.name}.png"
+            current_crypto_balance = get_current_balance(bar_plot_location=plot_filepath)
+            tweet = (
+                f"Jim's current crypto portfolio is worth "
+                + f"approximately ${round(current_crypto_balance, 2)}"
+            )
+            api.update_with_media(plot_filepath, status=tweet)
 
 
 if __name__ == "__main__":
