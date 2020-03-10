c6af5aeec8aae7f7e89c4c8b7c71dcdd7782fe4e
Jim Barrett <jimbarrett27@gmail.com>
2020-03-10 23:26:10 +0100

    solution for problem 2

--- website/app/static/goCode/main.go
+++ website/app/static/goCode/main.go
@@ -12,6 +12,8 @@ func solution(problemNumber int) float64 {
 	switch problemNumber {
 	case 1:
 		return float64(solutions.Problem1())
+	case 2:
+		return float64(solutions.Problem2())
 	default:
 		return -1
 	}
