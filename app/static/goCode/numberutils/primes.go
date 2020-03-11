6bbc95008b9b8f6e66c4a981fe4ee75a319c9464
Jim Barrett <jimbarrett27@gmail.com>
2020-03-11 18:27:42 +0100

    solution to problem 50

--- app/static/goCode/numberutils/primes.go
+++ app/static/goCode/numberutils/primes.go
@@ -1,26 +1,57 @@
 package numberutils
 
-func PrimesUpTo(max int) (primes []int) {
+import "math"
 
-	var sieve = make([]bool, max)
+func SieveUpTo(max int) (sieve []bool) {
+
+	sieve = make([]bool, max)
 	for i := range sieve {
 		sieve[i] = false
 	}
 
 	sieve[0] = true
 
-	primes = append(primes, 1)
-
 	for i := range sieve {
 		if sieve[i] == true {
 			continue
 		}
-		sieve[i] = true
-		primes = append(primes, i+1)
-		for j := i; j < max; j += i + 1 {
+		for j := i + i + 1; j < max; j += i + 1 {
 			sieve[j] = true
 		}
 	}
 
 	return
+}
+
+func PrimesUpTo(max int) (primes []int) {
+
+	sieve := SieveUpTo(max)
+
+	for i := range sieve {
+		if !sieve[i] {
+			primes = append(primes, i+1)
+		}
+	}
+
+	return
+
+}
+
+func IsPrime(primes []int, number int) bool {
+	maxTrial := int(math.Sqrt(float64(number)))
+
+	if number%2 == 0 {
+		return false
+	}
+
+	for i := range primes[1:] {
+		if number%primes[i] == 0 {
+			return false
+		}
+		if i > maxTrial {
+			break
+		}
+	}
+
+	return true
 }
