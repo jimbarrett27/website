ee3cb7fb36e43208fc44ec069f26aedb3cf91f56
Jim Barrett <jimbarrett27@gmail.com>
2020-03-11 14:54:50 +0100

    solution to problem 3

--- app/static/goCode/main.go
+++ app/static/goCode/main.go
@@ -14,6 +14,8 @@ func solution(problemNumber int) float64 {
 		return float64(solutions.Problem1())
 	case 2:
 		return float64(solutions.Problem2())
+	case 3:
+		return float64(solutions.Problem3())
 	default:
 		return -1
 	}
---
+++ app/static/goCode/solutions/problem3.go
@@ -0,0 +1,22 @@
+package solutions
+
+import (
+	"../numberutils"
+)
+
+func Problem3() (largestFactor int) {
+
+	var targetNumber int = 600851475143
+
+	// hacky
+	var primes = numberutils.PrimesUpTo(1000000)
+
+	for i := len(primes) - 1; i >= 0; i-- {
+		if targetNumber%primes[i] == 0 {
+			largestFactor = primes[i]
+			break
+		}
+	}
+
+	return
+}
