aa61d0923d45ff5086185152f8714a8f1262a791
Jim Barrett <jimbarrett27@gmail.com>
2020-03-15 13:36:15 +0100

    marginal speed improvement

--- website/app/static/goCode/solutions/problem5.go
+++ website/app/static/goCode/solutions/problem5.go
@@ -5,7 +5,7 @@ func Problem5() int {
 	outerCounter := 0
 OUTER:
 	for {
-		outerCounter += 16
+		outerCounter += 20
 		for i := 0; i < 20; i++ {
 			if outerCounter%(i+1) != 0 {
 				continue OUTER
