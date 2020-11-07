39ecd29506fe892b2000d61a6f3753d58743f09d
Jim Barrett <jimbarrett27@gmail.com>
2020-11-07 21:37:02 +0100

    solution code for problem 24

--- website/app/static/goCode/numberutils/bigNumbers.go
+++ website/app/static/goCode/numberutils/bigNumbers.go
@@ -19,3 +19,15 @@ func CountBigNumberDigits(num *big.Int) (nDigits int) {
 	nDigits = len(numStr)
 	return
 }
+
+func IntSliceToInt(intSlice []int) (value int) {
+
+	value = 0
+	decimal := 1
+	for i := len(intSlice)-1; i>=0; i-- {
+		value += decimal * intSlice[i]
+		decimal *= 10
+	}
+
+	return
+}
