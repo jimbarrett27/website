05bb01f3da2da7b6f61dab5dae8719f055912872
Jim Barrett <jimbarrett27@gmail.com>
2021-11-04 16:49:29 +0100

    reformat and lint

--- website/bots/hello_bot.py
+++ website/bots/hello_bot.py
@@ -1,9 +1,16 @@
-from telegram.ext import Updater
-from telegram.ext import CommandHandler
+"""
+A simple hello world bot to learn about bot deployment
+"""
+
+from telegram.ext import CommandHandler, Updater
 
 from bots.secrets import get_telegram_key
 
+
 def launch_hello_bot():
+    """
+    Launches a very simple telegram bot
+    """
 
     token = get_telegram_key()
 
@@ -15,7 +22,7 @@ def launch_hello_bot():
         text = f"jim, {' '.join(context.args)}"
         context.bot.send_message(chat_id=update.effective_chat.id, text=text)
 
-    caps_handler = CommandHandler('jim', jim)
+    caps_handler = CommandHandler("jim", jim)
     dispatcher.add_handler(caps_handler)
 
     updater.start_polling()
