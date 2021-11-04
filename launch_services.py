9d1a3039a0c152dcb37f555e13bee2be5daa4a09
Jim Barrett <jimbarrett27@gmail.com>
2021-11-04 16:52:13 +0100

    update the changelog

--- launch_services.py
+++ launch_services.py
@@ -2,12 +2,17 @@ from waitress import serve
 from app import app
 import logging
 import os
+from bots.hello_bot import launch_hello_bot
+
+
 
 logging.basicConfig(
     format='%(asctime)s %(levelname)-8s %(message)s',
     level=logging.INFO,
     datefmt='%Y-%m-%d %H:%M:%S' 
 )
+
+launch_hello_bot()
 
 port = os.environ.get('PORT')
 serve(app, port=port)
