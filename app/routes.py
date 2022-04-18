aa1510b2988f3801722742389960d11151ab777e
Jim Barrett <jimbarrett27@gmail.com>
2022-04-18 11:50:05 +0200

    make commands case insensitive

--- app/routes.py
+++ app/routes.py
@@ -152,6 +152,6 @@ def telegram_webhook(telegram_key: str):
     if not message["from"]["id"] == get_telegram_user_id():
         return ""
 
-    handle_bot_request(message["text"])
+    handle_bot_request(message["text"].lower())
 
     return ""
