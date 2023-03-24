5b7e276a03195444c13cf7fe58ee1cd54276c5ea
Jim Barrett <jimbarrett27@gmail.com>
2023-03-24 13:04:57 +0000

    reformat rust

--- app/static/code/solution_1.rs
+++ app/static/code/solution_1.rs
@@ -3,11 +3,10 @@ use std::fs;
 const DATA_PATH: &str = "app/static/data/advent_of_code/day_1.txt";
 
 pub fn part_1() -> i32 {
-    let input = fs::read_to_string(DATA_PATH)
-        .expect("Unable to read file");
+    let input = fs::read_to_string(DATA_PATH).expect("Unable to read file");
 
     let mut floor_count: i32 = 0;
-    
+
     for c in input.chars() {
         match c {
             '(' => floor_count += 1,
@@ -20,8 +19,7 @@ pub fn part_1() -> i32 {
 }
 
 pub fn part_2() -> i32 {
-    let input = fs::read_to_string(DATA_PATH)
-        .expect("Unable to read file");
+    let input = fs::read_to_string(DATA_PATH).expect("Unable to read file");
 
     let mut floor_count: i32 = 0;
     let mut char_count: i32 = 0;
@@ -34,7 +32,7 @@ pub fn part_2() -> i32 {
         }
 
         if floor_count == -1 {
-            break
+            break;
         }
     }
 
