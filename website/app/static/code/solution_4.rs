72a08d8435052a8b58b0a568c43cf9168a428908
Jim Barrett <jimbarrett27@gmail.com>
2023-03-25 21:36:10 +0100

    reformatting

--- website/app/static/code/solution_4.rs
+++ website/app/static/code/solution_4.rs
@@ -1,13 +1,11 @@
 use md5::{self, Digest};
 
 pub fn part_1() -> i32 {
-
-    let puzzle_input:String =  String::from("bgvyzdsv"); 
+    let puzzle_input: String = String::from("bgvyzdsv");
 
     let mut i: i32 = 100000;
     loop {
-
-        let numbers: String= i.to_string();
+        let numbers: String = i.to_string();
         let full_input: String = puzzle_input.clone() + &numbers;
 
         let digest: Digest = md5::compute(full_input.as_bytes());
@@ -15,24 +13,22 @@ pub fn part_1() -> i32 {
 
         let x = &as_str[..5];
 
-        if *x == String::from("00000") {break}
+        if *x == String::from("00000") {
+            break;
+        }
 
-        i+= 1;
-
+        i += 1;
     }
 
     i
-
 }
 
 pub fn part_2() -> i32 {
-
-    let puzzle_input:String =  String::from("bgvyzdsv"); 
+    let puzzle_input: String = String::from("bgvyzdsv");
 
     let mut i: i32 = 100000;
     loop {
-
-        let numbers: String= i.to_string();
+        let numbers: String = i.to_string();
         let full_input: String = puzzle_input.clone() + &numbers;
 
         let digest: Digest = md5::compute(full_input.as_bytes());
@@ -40,12 +36,12 @@ pub fn part_2() -> i32 {
 
         let x = &as_str[..6];
 
-        if *x == String::from("000000") {break}
+        if *x == String::from("000000") {
+            break;
+        }
 
-        i+= 1;
-
+        i += 1;
     }
 
     i
-
 }
