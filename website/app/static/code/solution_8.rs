44b7d74b1f1860641f05960df2611f9134d46857
Jim Barrett <jimbarrett27@gmail.com>
2023-03-31 18:20:47 +0200

    format rust

--- website/app/static/code/solution_8.rs
+++ website/app/static/code/solution_8.rs
@@ -1,5 +1,5 @@
-use std::fs;
 use regex::Regex;
+use std::fs;
 
 const DATA_PATH: &str = "app/static/data/advent_of_code/day_8.txt";
 
@@ -12,16 +12,14 @@ pub fn part_1() -> i32 {
     let mut string_characters_total: i32 = 0;
     let re = Regex::new(r#"\\"|\\\\|\\x[\da-f]{2}"#).unwrap();
     for line in lines {
-
         code_characters_total += line.len() as i32;
-        string_characters_total += line.len() as i32 -2;
-        
-        for matched_str in re.captures_iter(line){
-            string_characters_total -= matched_str[0].len() as i32 - 1;
+        string_characters_total += line.len() as i32 - 2;
 
+        for matched_str in re.captures_iter(line) {
+            string_characters_total -= matched_str[0].len() as i32 - 1;
         }
     }
-    
+
     (code_characters_total - string_characters_total) as i32
 }
 
@@ -34,16 +32,13 @@ pub fn part_2() -> i32 {
     let mut string_characters_total: i32 = 0;
     let re = Regex::new(r#""|\\"#).unwrap();
     for line in lines {
-
         string_characters_total += line.len() as i32 + 2;
         code_characters_total += line.len() as i32;
 
-        
-        for _ in re.captures_iter(line){
+        for _ in re.captures_iter(line) {
             string_characters_total += 1;
-
         }
     }
-    
+
     (string_characters_total - code_characters_total) as i32
 }
