ec2473f4df15bb6c53d32c05e1a0282914b566d3
Jim Barrett <jimbarrett27@gmail.com>
2025-03-30 23:02:05 +0200

    update infrastructure scripts

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,12 +6,19 @@ import sitemap from '@astrojs/sitemap';
 import rehypeMathjax  from 'rehype-mathjax';
 import remarkMath from 'remark-math';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	markdown: {
-		remarkPlugins: [remarkMath],
-		rehypePlugins: [rehypeMathjax],
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+
+  markdown: {
+      remarkPlugins: [remarkMath],
+      rehypePlugins: [rehypeMathjax],
 	  },
+
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
